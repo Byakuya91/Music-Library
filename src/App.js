705c42341e8043fff49fd95465e25f42a703e1b5
Byakuya91 <nganti91@yahoo.com>
2022-04-19 16:26:42 -0400

    I've successfully imported Axios and received the API data on songs. Currently working on displaying the table and have the headers up. The next step will be maping the songs JSON file to each of the headers and displaying that information.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import { useEffect } from "react";
+import axios from "axios";
+import SongTable from "./Components/SongTable/SongTable";
+
 function App() {
-  return <div className="App">Hello World! This is a test!</div>;
+  // state variables for the array of songs
+  const [songs, setSongs] = useState([]);
+  // create an UseEffect to activate the request
+  console.log("songs array is", songs);
+
+  useEffect(() => {
+    makeGetRequest();
+  }, []);
+
+  async function makeGetRequest() {
+    try {
+      let response = await axios.get(
+        "http://www.devcodecampmusiclibrary.com/api/music"
+      );
+
+      // setting the data to our state variable
+      setSongs(response.data);
+    } catch (err) {
+      console.log(err.message);
+    }
+  }
+
+  return (
+    <div className="App">
+      <SongTable />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Components/SongTable/SongTable.jsx
@@ -0,0 +1,27 @@
+const SongTable = (props) => {
+    return ( 
+      // create our display table
+      <table>
+          <thead>
+            <tr>
+               <th>id</th> 
+               <th>title</th> 
+               <th>album</th> 
+               <th>artist</th> 
+               <th>genre</th> 
+               <th>releaseDate</th> 
+            </tr>
+          </thead>
+          <tbody>
+              <tr>
+                  
+              </tr>
+          </tbody>
+      </table>
+
+
+
+     );
+}
+ 
+export default SongTable;
