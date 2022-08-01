630972d24e63090cbb5673f408e91b45fd144c95
Byakuya91 <nganti91@yahoo.com>
2022-08-01 17:13:54 -0400

    I've fixed the form component to allow songs to be added. But the songs are only stored locally. I will proceed into making so that way the songs are added to the API itself on the backend. I shall also explore the possibility of deleting songs from both the backend and the front-end.

--- src/Components/AddSong/AddSong.jsx
+++ src/Components/AddSong/AddSong.jsx
@@ -1,53 +1,69 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const AddSong = (props) => {
+  //  defining the state variables
+  const [title, setTitle] = useState("");
+  // console.log(title);
+  const [album, setAlbum] = useState("");
+  // console.log(album);
+  const [artist, setArtist] = useState("");
+  const [genre, setGenre] = useState("");
+  const [releaseDate, setReleaseDate] = useState("");
 
-    //  defining the state variables 
-    const [title, setTitle] = useState("");
-    // console.log(title);
-    const [album, setAlbum] = useState("");
-    // console.log(album);
-    const [artist, setArtist] = useState("");
-    const [genre, setGenre] = useState("");
-    const [releaseDate, setReleaseDate] = useState("");
+  //   create a handle submit function that will add the song to the SongTable.
+  function handleSubmit(formEvent) {
+    //    prevents the page from being reloaded.
+    formEvent.preventDefault();
 
-//   create a handle submit function that will add the song to the SongTable.
-     function handleSubmit(formEvent){
-        //    prevents the page from being reloaded. 
-          formEvent.preventDefault();
+    //   TODO: figure out how to grab form data
+    let newSongEntry = {
+      title: title,
+      album: album,
+      artist: artist,
+      genre: genre,
+      releaseDate: releaseDate,
+    };
+    // TODO: Some way to set it equal to our SongTable.
+    props.addNewSong(newSongEntry);
+    alert("Your song has been added!");
+  }
 
-        //   TODO: figure out how to grab form data 
-         let newSongEntry = {
-             title: title,
-             album: album,
-             artist: artist,
-             genre: genre,
-             releaseDate: releaseDate
-         };
-        // TODO: Some way to set it equal to our SongTable. 
-          props.AddSong(newSongEntry);
-          alert("Your song has been added!");
-     }
+  return (
+    //  Defining the form
+    <form onSubmit={handleSubmit}>
+      <label>title</label>
+      <input
+        type="text"
+        value={title}
+        onChange={(event) => setTitle(event.target.value)}
+      ></input>
+      <label>album</label>
+      <input
+        type="text"
+        value={album}
+        onChange={(event) => setAlbum(event.target.value)}
+      ></input>
+      <label>artist</label>
+      <input
+        type="text"
+        value={artist}
+        onChange={(event) => setArtist(event.target.value)}
+      ></input>
+      <label>genre</label>
+      <input
+        type="text"
+        value={genre}
+        onChange={(event) => setGenre(event.target.value)}
+      ></input>
+      <label>releaseDate</label>
+      <input
+        type="date"
+        value={releaseDate}
+        onChange={(event) => setReleaseDate(event.target.value)}
+      ></input>
+      <button type="submit">Add new song</button>
+    </form>
+  );
+};
 
-
-    return ( 
-        //  Defining the form 
-      <form onSubmit={handleSubmit}>
-          <label >title</label>
-          <input type = "text"  value={title} onChange={(event) => setTitle(event.target.value)}></input>
-          <label >album</label>
-          <input type = "text" value = {album} onChange={(event) => setAlbum(event.target.value)}></input>
-          <label>artist</label>
-          <input type = "text"  value = {artist} onChange={(event) => setArtist(event.target.value)} ></input>
-          <label>genre</label>
-          <input type = "text"   value = {genre} onChange={(event) => setGenre(event.target.value)} ></input>
-          <label>releaseDate</label>
-          <input type = "text"  value = {releaseDate} onChange={(event) => setReleaseDate(event.target.value)} ></input>
-          <button type='submit'>Add new song</button>
-
-      </form>
-
-     );
-}
- 
 export default AddSong;
